7087ccb2c1b7a027a81e36b7eaeb264dab34834d
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 18:38:28 +0530

    .

---
+++ P1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        System.out.println(5/0);
+		        System.out.println("End of program!");
+		        System.out.println("HEY!");
+				}
+}
+
+	
