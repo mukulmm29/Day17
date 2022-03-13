0e22c38fb37092b73aec9c8da317f59c0a03cccc
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 18:48:10 +0530

    .

---
+++ P6.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class P6 {
+	 public static void findFile() throws IOException {
+		    // code that may generate IOException
+		    File newFile = new File("test.txt");
+		    FileInputStream stream = new FileInputStream(newFile);
+		  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		      findFile();
+		    }
+		    catch (IOException e) {
+		      System.out.println(e);
+		    }
+	}
+
+}
